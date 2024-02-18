dacb55ee29f4f07f0905c204ee46d40c6b49f676
bibek376 <bibekrawat123@gmail.com>
2024-02-18 23:44:13 +0545

    process directly using pandas

---
+++ uber_data_engineering/data_loaders/rhapsodic_grass.py
@@ -0,0 +1,33 @@
+from mage_ai.io.file import FileIO
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_file(*args, **kwargs):
+    """
+    Template for loading data from filesystem.
+    Load data from 1 file or multiple file directories.
+
+    For multiple directories, use the following:
+        FileIO().load(file_directories=['dir_1', 'dir_2'])
+
+    Docs: https://docs.mage.ai/design/data-loading#fileio
+    """
+    # #filepath = ['/home/bibek/Desktop/uber_data_engineering/yellow_tripdata_2023-01.csv','/home/bibek/Desktop/uber_data_engineering/data/taxi_zones.csv']
+
+    # return FileIO().load(['/home/bibek/Desktop/uber_data_engineering/yellow_tripdata_2023-01.csv','/home/bibek/Desktop/uber_data_engineering/data/taxi_zones.csv'])
+    filepath = ['/home/bibek/Desktop/uber_data_engineering/yellow_tripdata_2023-01.csv', '/home/bibek/Desktop/uber_data_engineering/data/taxi_zones.csv']
+    data = []
+    for path in filepath:
+        data.append(FileIO().load(path))
+    return data
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
