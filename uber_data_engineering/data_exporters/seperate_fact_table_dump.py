c964bb660b7f80f66220d5378b61731ed02050ea
bibek376 <bibekrawat123@gmail.com>
2024-02-21 22:06:47 +0545

    pandas running using GPU

--- uber_data_engineering/data_exporters/seperate_fact_table_dump.py
+++ uber_data_engineering/data_exporters/seperate_fact_table_dump.py
@@ -1,3 +1,4 @@
+import os 
 from mage_ai.io.file import FileIO
 from pandas import DataFrame
 
@@ -6,11 +7,16 @@ if 'data_exporter' not in globals():
 
 
 @data_exporter
-def export_data_to_file(df: DataFrame, **kwargs) -> None:
+def export_data_to_file(data, **kwargs) -> None:
     """
     Template for exporting data to filesystem.
 
     Docs: https://docs.mage.ai/design/data-loading#fileio
     """
+    output_dir='/home/bibek/Desktop/uber_data_engineering/data'
+    for name,df in data:
+        
+
+
     filepath = 'path/to/write/dataframe/to.csv'
     FileIO().export(df, filepath)
